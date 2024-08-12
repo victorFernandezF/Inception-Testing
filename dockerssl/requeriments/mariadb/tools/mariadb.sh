a6f3abb427f1ec936d5a223952dd0f41f5075793
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-08-12 18:26:22 +0200

    test

--- dockerssl/requeriments/mariadb/tools/mariadb.sh
+++ dockerssl/requeriments/mariadb/tools/mariadb.sh
@@ -3,13 +3,10 @@
 # we have to start the mariadb so we can create the databases
 service mariadb start
 
-# Create the database if it doesn't exist
-mysql -uroot -p"$DB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE;"
-
-# Create the user if it doesn't exist and grant privileges
-mysql -uroot -p"$DB_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mysql -uroot -p"$DB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER'@'%';"
-mysql -uroot -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+mysql -e "create database if not exists $DB_DATABASE;"
+mysql -e "create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';"
+mysql -e "grant all privileges on $DB_DATABASE.* TO '$DB_USER'@'%';"
+mysql -e "flush privileges;"
 
 # kill any mysql daemon instance running
 pkill -f mysqld
