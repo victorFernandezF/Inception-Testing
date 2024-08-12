d6f7c0d2e65ebe4f2916766a7dcaabefafb24884
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-08-12 17:04:14 +0200

    try to fix mariadb

--- dockerssl/requeriments/mariadb/tools/mariadb.sh
+++ dockerssl/requeriments/mariadb/tools/mariadb.sh
@@ -1,46 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-#mysqld --bind-address=0.0.0.0
+# we have to start the mariadb so we can create the databases
+service mariadb start
 
-mysql_install_db
+mysql -e "create database if not exists $DB_DATABASE;"
+mysql -e "create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';"
+mysql -e "grant all privileges on $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -e "flush privileges;"
 
-/etc/init.d/mysql start || { echo "MariaDB no pudo iniciar"; exit 1; }
+# kill any mysql daemon instance running
+pkill -f mysqld
 
-#Check if the database exists
-
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-then 
-
-	echo "Database already exists"
-else
-
-# Set root option so that connexion without root password is not possible
-
-mysql_secure_installation << _EOF_
-
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
-
-#Add a root user on 127.0.0.1 to allow remote connexion 
-#Flush privileges allow to your sql tables to be updated automatically when you modify it
-#mysql -uroot launch mysql command line client
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Create database and user in the database for wordpress
-
-#echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-
-#Import database in the mysql command line
-#mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-
-#fi
-
-/etc/init.d/mysql stop
-
-exec "$@"
+# start the daemon again
+mysqld
