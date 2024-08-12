751805d48c4a8e4fcd73b1f23963b59acea75bfd
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-08-12 17:07:32 +0200

    fix error

--- dockerssl/requeriments/mariadb/tools/mariadb.sh
+++ dockerssl/requeriments/mariadb/tools/mariadb.sh
@@ -5,7 +5,7 @@ service mariadb start
 
 mysql -e "create database if not exists $DB_DATABASE;"
 mysql -e "create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';"
-mysql -e "grant all privileges on $DB_NAME.* TO '$DB_USER'@'%';"
+mysql -e "grant all privileges on $DB_DATABASE.* TO '$DB_USER'@'%';"
 mysql -e "flush privileges;"
 
 # kill any mysql daemon instance running
