4809718d3bd62a6d02e2c314ea9b15bf142e0eee
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-08-14 18:46:10 +0200

    kill pid

--- dockerssl/requeriments/mariadb/tools/mariadb.sh
+++ dockerssl/requeriments/mariadb/tools/mariadb.sh
@@ -7,7 +7,7 @@ chown -R mysql:mysql /run/mysqld
 service mariadb start
 
 # Wait for MariaDB to start up (optional, adjust time as needed)
-sleep 10
+sleep 100
 
 # Create the database if it doesn't exist
 mysql -uroot -p"$DB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE;"
@@ -16,6 +16,8 @@ mysql -uroot -p"$DB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB_DATABAS
 mysql -uroot -p"$DB_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -uroot -p"$DB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER'@'%';"
 mysql -uroot -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+
+pkill -f mysqld
 
 # Instead of killing and restarting mysqld, let it run in the foreground
 # To allow for this, don't stop the service; just leave it running
