07dbbcf05097f0a014315027867aaf5c8ca13c1f
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-08-30 17:09:43 +0200

    wp_testing

--- dockerssl/requirements/wordpress/tools/wordpress.sh
+++ dockerssl/requirements/wordpress/tools/wordpress.sh
@@ -1,54 +1,37 @@
 #!/bin/bash
 
 
+WP_CONFIG_FILE="/var/www/wordpress/wp-config.php"
+WP_CONFIG_SAMPLE="/var/www/wordpress/wp-config-sample.php"
+WP_CLI_URL="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-echo "inside script"
-mkdir /var/www/
-mkdir /var/www/html
+echo "SE EJECUTO EL SCRIPT"
 
-cd /var/www/html
+if ! [[ -f /var/www/wordpress/wp-config.php ]]; then
+    # Descargar WP-CLI
+    curl -o /usr/local/bin/wp $WP_CLI_URL
+    chmod +x /usr/local/bin/wp
 
+    # Copiar wp-config-sample.php a wp-config.php
+    cp $WP_CONFIG_SAMPLE $WP_CONFIG_FILE
 
-rm -rf *
+    # Configurar wp-config.php
+    sed -i "s/password_here/$DB_PASSWORD/g" $WP_CONFIG_FILE
+    sed -i "s/localhost/mariadb.srcs_inception/g" $WP_CONFIG_FILE
+    sed -i "s/username_here/$DB_USER/g" $WP_CONFIG_FILE
+    sed -i "s/database_name_here/$DB_NAME/g" $WP_CONFIG_FILE
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+    # Instalar WordPress
+    wp core install --allow-root --url=$URL --title=$DB_USER \
+        --admin_user=$DB_USER --admin_password=$DB_PASSWORD \
+        --admin_email=$DB_EMAIL --path=/var/www/wordpress
 
-chmod +x wp-cli.phar 
+    # Crear usuario adicional
+    wp user create $DB_USER2 $DB_EMAIL2 --user_pass=$DB_PASSWORD2 \
+        --role=author --allow-root --path=/var/www/wordpress
+else
+    echo "El archivo wp-config.php ya existe."
+fi
 
-mv wp-cli.phar /usr/local/bin/wp
-
-
-
-wp core download --allow-root
-
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-mv /wp-config.php /var/www/html/wp-config.php
-
-
-sed -i -r "s/db1/$db_name/1"   wp-config.php
-sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
-
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-wp theme install astra --activate --allow-root
-
-wp plugin install redis-cache --activate --allow-root
-
-wp plugin update --all --allow-root
-
-
- 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-
-
-wp redis enable --allow-root
-
-/usr/sbin/php-fpm7.3 -F
+# Iniciar el servidor PHP-FPM (usando la ruta completa)
+/usr/sbin/php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
