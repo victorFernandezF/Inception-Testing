b68a5d516922ffcccbf5a099ce6c2083fc956b92
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-09-13 11:47:41 +0200

    scripty

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,29 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-WP_CONFIG_FILE="/var/www/wordpress/wp-config.php"
-WP_CONFIG_SAMPLE="/var/www/wordpress/wp-config-sample.php"
-WP_CLI_URL="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
-
-if ! [[ -f /var/www/wordpress/wp-config.php ]]; then
-    curl -o /usr/local/bin/wp $WP_CLI_URL
-    chmod +x /usr/local/bin/wp
-
-    cp $WP_CONFIG_SAMPLE $WP_CONFIG_FILE
-
-    sed -i "s/password_here/$DB_PASSWORD/g" $WP_CONFIG_FILE
-    sed -i "s/localhost/mariadb.srcs_inception/g" $WP_CONFIG_FILE
-    sed -i "s/username_here/$DB_USER/g" $WP_CONFIG_FILE
-    sed -i "s/database_name_here/$DB_NAME/g" $WP_CONFIG_FILE
-
-    wp core install --allow-root --url=$URL --title=$DB_USER \
-        --admin_user=$DB_USER --admin_password=$DB_PASSWORD \
-        --admin_email=$DB_EMAIL --path=/var/www/wordpress
-
-    wp user create $DB_USER2 $DB_EMAIL2 --user_pass=$DB_PASSWORD2 \
-        --role=author --allow-root --path=/var/www/wordpress
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
 else
-    echo "wp-config file already"
+
+####### MANDATORY PART ##########
+
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+
+	#Inport env variables in the config file
+	sed -i "s/username_here/$DB_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$DB_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$DB_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$DB_NAME/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+###################################
 fi
 
-# Iniciar el servidor PHP-FPM (usando la ruta completa)
-/usr/sbin/php-fpm7.4 -y /etc/php/7.4/fpm/php-fpm.conf -F
+exec "$@"
