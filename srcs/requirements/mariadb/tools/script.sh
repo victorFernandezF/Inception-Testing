f5bd2549bc9e1b0f61d1e7402666895660c1d445
Víctor Fernández Flores <victorfernandezflores2@gmail.com>
2024-10-09 12:05:50 +0200

    test

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Iniciar el servicio de MariaDB
 service mariadb start 
 
+# Esperar un momento para que el servicio se inicialice completamente
+sleep 5
+
+# Ejecutar los comandos SQL
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS '$DB_USER@wordpress.srcs_incetion' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "ALTER USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "FLUSH PRIVILEGES;"
-
-tail -f /dev/null
